8ce57ac2a3657ef7d0083b14b56e559a0c3191b7
eugeneodhiambo <eugeneodhiambo07@gmail.com>
2022-06-30 02:34:47 -0700

    Piece of art

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main prints the stated sentence
+ * return 0 if exited well
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);	
+}
