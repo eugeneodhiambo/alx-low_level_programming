8b8af96235baef4648b6a425ad8b1868999e0a27
eugeneodhiambo <eugeneodhiambo07@gmail.com>
2022-06-30 02:20:21 -0700

    Correction to task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main(void)
-
+/* Main will print Programming is like building a multilingual puzzle if all is okay.Returns 0 if all goes well otherwise returns non-zero
+ */
 {
    puts("\"Programming is like building a multilingual puzzle");
    return (0);
