730160a412d4bce7d90f04e17d25ad866cb4f6c1
eugeneodhiambo <eugeneodhiambo07@gmail.com>
2022-07-01 11:57:24 -0700

    print with comas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints a series of numbers with decimal
+ *
+ * Return: Always(success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= 9; c++)
+		putchar(c);
+	        if (c !='9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar(c);
+		return (0);
+}
+
