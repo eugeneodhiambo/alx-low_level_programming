aad779e44c933bc794550f360c466c3718533c18
eugeneodhiambo <eugeneodhiambo07@gmail.com>
2022-07-01 08:50:40 -0700

    Print lower case alphabet followed by upercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,17 +3,17 @@
 /**
  * main - prints alphabets in lowecase followed by uppercase
  *
- * Return - Always(success)
+ * Return:  Always(success)
  */
 
 int main(void)
 {
 	char c;
 
-	for(c = 'a'; c <= 'z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 		putchar(c);
 
-	for(c = 'A'; c <= 'Z'; c++)
+	for (c = 'A'; c <= 'Z'; c++)
 		putchar(c);
 
 	putchar('\n');
