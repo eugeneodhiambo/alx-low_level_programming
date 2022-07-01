b87969c5eb648cddba4d253ffe4bc33fb99eb9ee
eugeneodhiambo <eugeneodhiambo07@gmail.com>
2022-07-01 12:11:50 -0700

    print with comas

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,14 +8,15 @@
 int main(void)
 {
 	char c;
-
 	for (c = '0'; c <= 9; c++)
+	{
 		putchar(c);
-	        if (c !='9')
+		if (c != '9')
 		{
 			putchar(',');
 			putchar(' ');
 		}
+	}
 	putchar(c);
 	return (0);
 }
