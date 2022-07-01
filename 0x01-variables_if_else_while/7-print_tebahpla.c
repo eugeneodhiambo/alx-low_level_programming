69d3d6837419ff0335edc75c124602b95003ef8d
eugeneodhiambo <eugeneodhiambo07@gmail.com>
2022-07-01 11:35:16 -0700

    Print digits from 0 to 9 without using char

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prin in reverse
+ *
+ * Return: Always(success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c++)
+		putchar(c);
+	putchar('\n');
+
+	return (0);
+}
