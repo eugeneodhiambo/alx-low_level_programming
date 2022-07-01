c4641a3ac713de0b60cc5fc5b3d0108e08bf5520
eugeneodhiambo <eugeneodhiambo07@gmail.com>
2022-07-01 11:27:16 -0700

    Print digits from 0 to 9 without using char

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,8 +9,8 @@ int main(void)
 {
 	int number;
 
-	for (number = '0'; number < '9'; number++)
+	for (number = '0'; number <= '9'; number++)
 		putchar(number);
 	putchar('\n');
-	return(0);
+	retur (0);
 }
