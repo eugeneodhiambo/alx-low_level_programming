c1fa6c135c8f97f9cd1da464a8d8e04c9c2e6acb
eugeneodhiambo <eugeneodhiambo07@gmail.com>
2022-07-01 08:09:14 -0700

    positive negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,7 +15,6 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
-
 		printf("%d is positive\n", n);
 
 	else if (n == 0)
@@ -23,7 +22,6 @@ int main(void)
 		printf("%d is zero\n", n);
 	}
 	else 
-
 		printf("%d is negative\n", n);
 
 	return (0);
