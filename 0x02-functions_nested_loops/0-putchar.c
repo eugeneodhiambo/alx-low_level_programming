4c2e810edc083c1ad0094d244477f62db371964a
eugeneodhiambo <eugeneodhiambo07@gmail.com>
2022-07-05 22:48:11 -0700

    Add new line code

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,7 +5,8 @@
  *
  * Return: Always 0
  */
-int main(void){
+int main(void)
+{
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -14,6 +15,7 @@ int main(void){
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
+	_putchar('\n');
 
-	return 0;
+	return (0);
 }
