13792fda44225cf17e0f680a74d62c855526dbe9
eugeneodhiambo <eugeneodhiambo07@gmail.com>
2022-07-06 00:05:15 -0700

    Add task 6 to 0x002

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,12 +5,12 @@
  * @n: the int to check
  * Return: the absolute value of int
  */
-int int _abs(int n)
+int _abs(int n)
 {
 	if (n >= 0)
 	{
-		return (n)
+		return (n);
 	}
-	return (-n)
+	return (-n);
 }
 
