1deede50f21f4460af863f901bb40da7c9e1c3da
eugeneodhiambo <eugeneodhiambo07@gmail.com>
2022-07-06 00:22:25 -0700

    Added task 7 to 0x002

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,17 @@
  */
 int print_last_digit(int n)
 {
-	int l, p;
+        int a;
 
-	l = n % 10;
-	if (l < 10)
-		l = -l;
-	p = '0' + 1;
-	_putchar(p);
-	return (l);
+        if (n < 0)
+		n = -n;
+	
+	a = n % 10;
+
+        if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
 }
