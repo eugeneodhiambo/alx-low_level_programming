8429138c97424cdf2630cb1b61adc238357f4180
eugeneodhiambo <eugeneodhiambo07@gmail.com>
2022-07-10 15:12:11 -0700

    Added 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,14 +13,13 @@ void more_numbers(void)
 	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
 			{
-				if (j >= 10)
-				{
-					_putchar('1');
-					_putchar(j % 10 + '0');
-				}
-				_putchar('\n');
+				_putchar('1');
+				_putchar(j % 10 + '0');
 			}
+			_putchar('\n');
 		}
 	}
 }
