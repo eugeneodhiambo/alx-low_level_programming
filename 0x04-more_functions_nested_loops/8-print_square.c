249b31efba549b5a8f9ac6957909e583df3079cb
eugeneodhiambo <eugeneodhiambo07@gmail.com>
2022-07-11 08:35:26 -0700

    Added task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -12,7 +12,7 @@ void print_square(int size)
 	if (size > 0)
 		for (height = 0; height < size; height++)
 		{
-			for (width 0; width < size; width++)
+			for (width = 0; width < size; width++)
 				_putchar('#');
 			if (height == size - 1)
 				continue;
