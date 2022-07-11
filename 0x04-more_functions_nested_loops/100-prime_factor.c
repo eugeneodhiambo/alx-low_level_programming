6c9c563307aad38da1d3606196d7ef54892e0f50
eugeneodhiambo <eugeneodhiambo07@gmail.com>
2022-07-11 12:29:29 -0700

    Added task 100

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -20,7 +20,6 @@ int main(void)
 		max = 2;
 		n /= 2;
 	}
-
 	for (i = 3; i <= sqrt(n); i = i + 2)
 	{
 		while (n % i == 0)
@@ -32,4 +31,7 @@ int main(void)
 	if (n > 2)
 		max = n;
 	printf("%ld\n", max);
+
+	return (0);
 }
+
