52c6c18577eff99798bc452784e012987d824cf3
eugeneodhiambo <eugeneodhiambo07@gmail.com>
2022-07-14 12:46:39 -0700

    Added tak 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,27 +1,17 @@
 #include "mainn.h"
 
-
-
 /**
-
  * _strncpy - copies a string
-
  * @dest: destination string
-
  * @src: source string
-
  * @n: number of bytes to copy
-
  *
-
  * Return: pointer to the resulting string
-
  */
 
 char *_strncpy(char *dest, char *src, int n)
 
 {
-
 	int i;
 
 
