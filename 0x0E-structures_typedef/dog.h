2115ca76145c56bcc9bbefa055233971c5bb36f5
eugeneodhiambo <eugeneodhiambo07@gmail.com>
2022-08-01 21:51:24 -0700

    Added task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - dog class
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
+struct dog 
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
