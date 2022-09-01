3c60a79f3b352f3322ec0534f7d6f521134b7a14
eugeneodhiambo <eugeneodhiambo07@gmail.com>
2022-09-01 02:55:32 -0700

    added task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * main - copies the content of a file to another file
  * @argc: number of arguments passed to the program
@@ -8,16 +7,13 @@
  */
 int main(int argc, char *argv[])
 {
-
 	int fd_r, fd_w, r, a, b;
 	char buf[BUFSIZ];
-
 
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
-
 	}
 	fd_r = open(argv[1], O_RDONLY);
 	if (fd_r < 0)
@@ -28,7 +24,6 @@ int main(int argc, char *argv[])
 	fd_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	while ((r = read(fd_r, buf, BUFSIZ)) > 0)
 	{
-
 		if (fd_w < 0 || write(fd_w, buf, r) != r)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
@@ -46,12 +41,10 @@ int main(int argc, char *argv[])
 	b = close(fd_w);
 	if (a < 0 || b < 0)
 	{
-
 		if (a < 0)
 			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_r);
 		if (b < 0)
 			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_w);
 		exit(100);
-	}
-	return (0);
+	}	return (0);
 }
